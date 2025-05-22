e65e1a1ea13eb55751df2e2e1c0a7a1fdf3d1c6a
Soyivan24 <105176232+Soyivan24@users.noreply.github.com>
2025-05-21 23:53:51 -0500

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -2,4 +2,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabaseUrl = 'https://vzwdgxxnjrvnoxxozcrs.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6d2RneHhuanJ2bm94eG96Y3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4ODc2MTksImV4cCI6MjA2MzQ2MzYxOX0.ONv_NwyA1C7gOKFrUkh7uj4IG8g33ufVhW-VbON1UEg';
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
